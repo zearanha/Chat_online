80fae9262d22a44591583282bea1d5687696719a
zearanha <JAranha21@gmail.com>
2025-05-08 01:36:25 -0300

    Versão 1 original chat online

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import { WebSocketServer } from 'ws';
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+
+const wss = new WebSocketServer({ port:process.env.PORT || 8080 });
+
+wss.on("connection", (ws) => {
+    ws.on('error', console.error);
+
+    ws.on("message", (data) =>{
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+
+    console.log("Client connected")
+})
